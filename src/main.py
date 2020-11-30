5096dbc154eafca2126b68c5c5366f5d1c383888
Igor Fernandes Miranda <igormiranda5@gmail.com>
2020-11-30 11:16:25 -0300

    Updated main

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ def main():
     print("Mining data")
     states = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RR", "RO", "RJ", "RN", "RS", "SC", "SP", "SE", "TO"]
     for state in states:
-        miner = Miner(state, datafolder="result")
-        miner.mine()
+        try:
+            miner = Miner(state, datafolder="result")
+            miner.mine()
+        except Exception as e:
+            print("Error mining state " + state + "\n")
+            print(e)
+
     print("End miner")
 
 if __name__ == "__main__":
